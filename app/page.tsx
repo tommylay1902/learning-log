bc3fe6339b8d1b1a84db5ff63fe6e8cee114b12d
tommylay1902 <tommylay.c@gmail.com>
2025-07-21 12:35:02 +0000

    init

---
+++ app/page.tsx
@@ -0,0 +1,28 @@
+import { AuthButton } from "@/components/auth-button";
+import TimeLine from "@/components/timeline";
+import { Button } from "@/components/ui/button";
+import { NavigationMenu } from "@/components/ui/navigation-menu";
+import { createClient } from "@/lib/supabase/server";
+import { NavigationMenuList } from "@radix-ui/react-navigation-menu";
+
+export default async function Home() {
+  const supabase = await createClient();
+  const { data: log } = await supabase.from("log").select();
+  const { data, error } = await supabase.auth.getClaims();
+
+  return (
+    <>
+      <div className="flex justify-end w-full p-4">
+        <NavigationMenu>
+          <NavigationMenuList>
+            <AuthButton />
+          </NavigationMenuList>
+        </NavigationMenu>
+      </div>
+      <main className="min-h-screen flex flex-col items-center">
+        <TimeLine entries={log} />
+        {!error && data && <Button>Add Log </Button>}
+      </main>
+    </>
+  );
+}
---
+++ components/timeline.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { Card, CardContent } from "./ui/card";
+import { Separator } from "./ui/separator";
+import { Database } from "@/database.types";
+
+interface TimelineProps {
+  entries: Database["public"]["Tables"]["log"]["Row"][] | null;
+}
+const TimeLine: React.FC<TimelineProps> = ({ entries }) => {
+  const conversionDate = (dateString: string) => {
+    const date = new Date(dateString);
+    return date.toLocaleDateString("en-US", {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    });
+  };
+  return (
+    <section className="bg-background py-32">
+      <div className="container mx-auto py-2">
+        <h1 className="text-foreground mb-10 text-center text-3xl font-bold tracking-tighter sm:text-6xl">
+          Tommy Lay&apos;s Learning Log
+        </h1>
+        <div className="relative mx-auto max-w-4xl">
+          <Separator
+            orientation="vertical"
+            className="bg-muted absolute left-2 top-4"
+          />
+          {entries !== null &&
+            entries.map((entry, index) => (
+              <div key={index} className="relative mb-10 pl-8">
+                <div className="bg-foreground absolute left-0 top-3.5 flex size-4 items-center justify-center rounded-full" />
+
+                <div className="flex h-5 items-center space-x-4 text-2xl font-bold">
+                  <div>{conversionDate(entry.created_at.split("T")[0])}</div>
+
+                  <Separator orientation="vertical" />
+                  <div>{entry.title}</div>
+                </div>
+
+                <Card className="my-5 border-none shadow-none">
+                  <CardContent className="px-0 xl:px-2">
+                    <div className="flex items-center space-x-5 pt-2 flex-shrink-0">
+                      <div className="flex-shrink-0 pt-2 font-semibold">
+                        {entry.time_spent} Pomos
+                      </div>
+                      <Separator
+                        orientation="vertical"
+                        className="min-h-full"
+                      />
+                      <div className="pt-2"> {entry.content}</div>
+                    </div>
+                  </CardContent>
+                </Card>
+              </div>
+            ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default TimeLine;
