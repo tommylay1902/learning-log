1c1b8a218ea27e443c2cf7d96c98e0bab5e4e1cb
tommylay1902 <tommylay.c@gmail.com>
2025-08-15 05:37:23 -0700

    moved skeleton into a seperate folder under components, loading spinner for timeline

--- app/learning-log/_ui/_views/timeline-view.tsx
+++ app/learning-log/_ui/_views/timeline-view.tsx
@@ -4,6 +4,7 @@ import TimeLine from "../_components/timeline";
 import { createClient } from "@/lib/supabase/client";
 import { cookies } from "next/headers";
 import { fetchLogs, fetchWeeklyLogs } from "../../_server/logs";
+import { LoadingSpinner } from "@/components/ui/loading-spinner";
 
 const TimeLineView = async () => {
   const supabase = createClient();
@@ -15,7 +16,16 @@ const TimeLineView = async () => {
 
   return (
     <ErrorBoundary fallback={<p>Error...</p>}>
-      <Suspense fallback={<p>Loading...</p>}>
+      <Suspense
+        fallback={
+          <div className="flex flex-col justify-center items-center mt-[10dvh]">
+            <div>
+              <LoadingSpinner className="w-[10dvw] h-[10dvh]  text-center" />
+            </div>
+            <p className="text-muted-foreground">Getting learning logs...</p>
+          </div>
+        }
+      >
         <TimeLine
           fetchLogs={fetchLogsCall}
           fetchWeeklyLogs={fetchWeeklyLogsCall}
